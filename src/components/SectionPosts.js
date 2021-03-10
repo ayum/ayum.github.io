e4d7a0b9b79788c114d8196da02060f17127b5d0
Stackbit Code Editor <support@stackbit.com>
2021-03-10 10:40:25 +0000

    SectionPosts.js: changed

--- src/components/SectionPosts.js
+++ src/components/SectionPosts.js
@@ -19,7 +19,7 @@ export default class SectionPosts extends React.Component {
                 {_.map(_.orderBy(display_posts, 'frontmatter.date', 'desc'), (post, post_idx) => (
                 <div className="article-teaser">
                 <Link key={post_idx} to={withPrefix(_.get(post, 'url', null))}>
-                    {_.get(post, 'frontmatter.thumb_img_href', null) ? <a href={_.get(post, 'frontmatter.thumb_img_href', null)}>{post_thumb(post)}</a></object> : post_thumb(post)}
+                    {_.get(post, 'frontmatter.thumb_img_href', null) ? <a href={_.get(post, 'frontmatter.thumb_img_href', null)}>{post_thumb(post)}</a> : post_thumb(post)}
                     <div className="copy">
                         <h2>{_.get(post, 'frontmatter.title', null)}</h2>
                         <h3 className="publish-date">Опубликовано {moment(_.get(post, 'frontmatter.date', null)).strftime('%d %B %Y')}</h3>
