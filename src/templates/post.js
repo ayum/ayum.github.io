6e3ff238e1352dd1d80ebf5a7292b465b5101b94
Stackbit Code Editor <support@stackbit.com>
2021-03-11 13:46:17 +0000

    post.js: changed

--- src/templates/post.js
+++ src/templates/post.js
@@ -39,7 +39,7 @@ export default class Post extends React.Component {
                 <div className="content">
                     <p>{_.get(this.props, 'pageContext.frontmatter.excerpt', null)}</p>
                     <div className="article-teaser">
-<img class="thumbnail" src={_.get(this.props, 'pageContext.frontmatter.thumb_img_path', null)} alt={_.get(this.props, 'pageContext.frontmatter.thumb_img_path', null)}/>
+<img class="thumbnail" src={_.get(this.props, 'pageContext.frontmatter.thumb_img_path', null)} alt={_.get(this.props, 'pageContext.frontmatter.thumb_img_alt', null)}/>
 <p><a href="/bumf/sobraniya.pdf">Скачать в формате <code>pdf</code></a></p>
 <p><a href="/bumf/sobraniya.txt">Скачать в формате <code>txt</code></a></p>
 </div>
