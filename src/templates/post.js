6b4d049f1600af2db44ab7f32981c085f4772112
Stackbit Code Editor <support@stackbit.com>
2021-03-11 13:42:15 +0000

    post.js: changed

--- src/templates/post.js
+++ src/templates/post.js
@@ -37,7 +37,7 @@ export default class Post extends React.Component {
                     </div>
                 </header>
                 <div className="content">
-                    <p>{_.get(this.props, 'pageContext.frontmatter.excerpt', null) && ()</p>
+                    <p>{_.get(this.props, 'pageContext.frontmatter.excerpt', null) && (_.get(this.props, 'pageContext.frontmatter.excerpt', null))}</p>
                     {htmlToReact(_.get(this.props, 'pageContext.html', null))}
                 </div>
             </section>
